588692c596da7673bf887616589a644997c04ee3
twilligon <git@twilligon.com>
2026-05-13 22:54:19 -0700

    Bump version

--- claude_backup/__init__.py
+++ claude_backup/__init__.py
@@ -41,7 +41,7 @@ from aiohttp import (
 from yarl import URL
 import browser_cookie3  # pyright: ignore[reportMissingTypeStubs]
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = (
     "__version__",
