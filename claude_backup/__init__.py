49092528be5dc8f7ad01ace0c6fc35ed9882c19d
twilligon <git@twilligon.com>
2026-09-04 14:03:43 -0700

    Bump version

--- claude_backup/__init__.py
+++ claude_backup/__init__.py
@@ -1,3 +1,3 @@
 """Unofficial, unsanctioned tool to back up Claude.ai chats to local files."""
 
-__version__ = "0.1.12"  # NB: remember to update MIGRATIONS on version bump!
+__version__ = "0.1.13"
